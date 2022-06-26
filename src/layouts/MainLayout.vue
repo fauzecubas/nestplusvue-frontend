c0389a7daf36900039cf0558ae83f93bc119d9d1
Fauze Cubas <fauzehenrique01@gmail.com>
2022-06-26 17:20:25 -0300

    feat: MainLayout.vue

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,31 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header color="secondary" elevated>
+      <q-toolbar>
+        <q-toolbar-title style="font-family: monospace">
+          CRUD App - nestplusvue
+        </q-toolbar-title>
+
+        <div style="font-family: monospace">by Fauze Cubas</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {},
+
+  setup() {
+    return {};
+  },
+});
+</script>
