fd9e8092f80ab1310d7f5d88b81807a5599a01fc
Fauze Cubas <fauzehenrique01@gmail.com>
2022-06-29 22:56:11 -0300

    feat: formatDate util

--- src/util/index.js
+++ src/util/index.js
@@ -1,2 +1,8 @@
+import moment from 'moment';
+
 export const formatZipCode = (zipCode) =>
   `${zipCode.substring(0, 5)}-${zipCode.substring(5)}`;
+
+export function formatDate(date) {
+  return moment(date, 'YYYY-MM-DD hh:mm:ss A Z').format('DD/MM/YYYY - HH:mm');
+}
